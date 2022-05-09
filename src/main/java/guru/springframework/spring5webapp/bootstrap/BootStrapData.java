df35105d60fa43094f8c4880410d368afd7b03b3
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-05-09 16:37:16 +0200

    Remove imports

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -9,9 +9,6 @@ import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.stream.Collectors;
-
 /**
  * Created by jt on 12/23/19.
  */
